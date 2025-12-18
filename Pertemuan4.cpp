378599f2e3bf9282056c9d9717934ebf7bcf537b
Alriando Saputra <alriandosaputra2020@gmail.com>
2025-12-18 23:19:09 +0700

    Add loop examples with continue and break usage
    Demonstrate for, while, and do-while loops with examples. Include usage of continue and break statements in for loops.

---
+++ Pertemuan4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "For loop output" << endl;
+    for(int i = 0; i < 5; i++ ) {
+        cout << "Nilai i = " << i << " ";
+    }
+    cout << endl;
+
+    cout << "While loop output" << endl;
+    int a = 0;
+    while(a < 5) {
+        cout << "Nilai i = " << a << " ";
+        a++;
+    }
+    cout << endl;
+
+    cout << "Do-While loop output" << endl;
+    int b = 0;
+    do {
+        cout << "Nilai i = " << b << " ";
+        b++;
+    } while(b < 5);
+    cout << endl;
+
+    cout << "Penggunaan continue di for loop" << endl;
+        for (int i = 0; i < 10; i++) {
+            if (i==5) {
+                continue;
+            }
+            cout << "Nilai i = " << i << " ";
+        }
+        cout << endl;
+
+    cout << "Penggunaan break di for loop" << endl;
+    for (int i = 0; i < 10; i++) {
+        if(i == 5) {
+            break;
+        }
+        cout << "Nilai i = " << i << " ";
+    }
+    cout << endl;
+
+    cout << "Program selesai" << endl;
+    return 0;
+ 
+
+
+}
