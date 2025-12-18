cd1454ef2ad48f69a0e1ad85f8982ee236a9d8ba
Alriando Saputra <alriandosaputra2020@gmail.com>
2025-12-18 23:15:42 +0700

    Create Pertemuan1.cpp

---
+++ Pertemuan1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Alriando Saputra";
+    string NPM = "2517051057";
+
+    cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+    cout << "Nama Saya Adalah " << nama << endl;
+    cout << "NPM Saya Adalah " << NPM << endl << endl;
+
+    cout << "UNILA Indah" << endl;
+    cout << "Be Strong" << endl;
+    return 0;
+}
